d737b2a623a0114edf3452a8e569354e343946f5
farhanfadilla <farhanfadilla86@gmail.com>
2020-10-13 23:28:29 +0700

    ganti key

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 CONSUMER_KEY = "FKUCGd0vcCL5vi6CaYFn9TJmf"
 CONSUMER_SECRET = "MGxOWwJUZMBQAef1XJbrbKNJisbHNu26bsKErHSczbf5R3kRtJ"
-ACCESS_KEY = "1315998691430469634-qd080enIfRlDUaakz7O9TRXuT8s137"
-ACCESS_SECRET = "X1E5cWnnxDr5Vl2K0ypWSm9gUL8zzV68M4b4feqE3lj3G"
+ACCESS_KEY = "1315998691430469634-2VJAlB3qp7HZsCxt3gFt8G0mx8Ep5N"
+ACCESS_SECRET = "I7EfvNgBBPIg7A5hK6PuxuBnZZeHPi09kCGW7iG3rbnzt"
 Github_username = "farhanfadilla"
 Github_password = "M4nchestercity123"
 Github_repo = "farhanfadilla/Database_UINJKTFESS" #use another repo instead of primary repo
@@ -9,6 +9,4 @@ First_Keyword = "-uwin" #primary keyword for tweet, vide, and image
 Second_Keyword = "[quote]" #for make image quoted
 Sub2_Keyword = "-s" #give image quoted name
 Third_keyword = "[tamin]" #ask admin
-Admin_id = "farhan_fadillaa" 
-
-#test heroku
+Admin_id = "farhan_fadillaa"
